b178d52ef716bb030979f278de63385e8aa8344b
M1guelpereira0797 <miguelpereira1242@gmail.com>
2026-08-04 10:26:29 -0400

    04/08/2026
    Practicando los operadores logicos y los condicionales

---
+++ Consola/Program.cs
@@ -0,0 +1,61 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+/*Console.WriteLine("Hello, World!");
+
+
+Console.WriteLine("Ingrese el primer numero que desee sumar: ");
+int num1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Ingrese el segundo numero que desee sumar: ");
+int num2 = int.Parse(Console.ReadLine());
+
+int suma  = num1 + num2;
+if(num1 > 0 && num2 >0)
+{
+
+    Console.WriteLine("El resultado de la suma es:  " + suma);
+}
+else
+{
+    Console.WriteLine("Los numeros ingresado son iguales a 0 o menores a 0 ");
+}
+*//*
+List<int> Numeros = new List<int>();
+
+Console.WriteLine("Ingrese la cantidad de alumnos que desee evaluar: ");
+int alumnos = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 0; i < alumnos; i++)
+{
+    Console.WriteLine("Ingrese la nota del alumno: ");
+    int nota = Convert.ToInt32(Console.ReadLine());
+    Numeros.Add(nota);
+}
+
+Console.WriteLine("Notas ingresadas de los alumnos evaluados: ");
+foreach(int n in Numeros)
+{
+    Console.WriteLine(n);
+}*/
+
+  Dictionary<string, int> Evaluados =  new Dictionary<string, int>();
+
+Console.WriteLine("Ingrese la cantidad de alumnos que desee evaluar: ");
+int alumnos = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 0; i < alumnos; i++) { 
+
+    Console.WriteLine("Ingrese el nombre del alumno: ");
+    string nombre = Console.ReadLine();
+    Console.WriteLine("Ingrese la nota del alumno: ");
+    int nota = Convert.ToInt32(Console.ReadLine());
+    Evaluados.Add(nombre, nota);
+}
+
+Console.WriteLine("Notas ingresadas de los alumnos evaluados: ");
+foreach (var n in Evaluados)
+{
+    Console.WriteLine($"{n.Key} = {n.Value}");
+}
+
+Console.ReadKey();
