2bb3e9a41d20f12a9b10bd1296c6eae6a83ab168
M1guelpereira0797 <miguelpereira1242@gmail.com>
2026-08-19 14:16:17 -0400

    Clases
    Diferencias entre una clase con get y set, y con constructor

---
+++ Clases/Program.cs
@@ -0,0 +1,17 @@
+﻿public class Persona
+{
+    // Propiedades privadas con get y set
+    private string Nombres { get; set; }
+    private int Edad { get; set; }
+    private string Direccion { get; set; }
+    private string Apellidos { get; set; }
+
+    // Constructor que obliga a inicializar todos los campos
+    public Persona(string nombres, int edad, string direccion, string apellidos)
+    {
+        this.Nombres = nombres;
+        this.Edad = edad;
+        this.Direccion = direccion;
+        this.Apellidos = apellidos;
+    }
+}
